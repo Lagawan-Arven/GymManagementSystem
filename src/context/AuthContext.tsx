e49abed66d2ee9e863158847eda63c03d0e5f6f4
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-16 18:51:17 +0800

    added interfaces and signup function

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,48 +1,90 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { signinUser } from "../services/api/authService";
+import { signinUser, signupUser } from "../services/api/authService";
 import { useNavigate } from "react-router-dom";
 
+interface SignupPayload {
+  name: string;
+  age: number;
+  sex: string;
+  email: string;
+  password: string;
+}
+
+interface SigninPayload {
+  emailUsername: string;
+  password: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  age: number;
+  sex: string;
+  username: string;
+  email: string;
+}
+
 interface AuthContextType {
-  user: string | null;
-  signin: (email: string, password: string) => Promise<void>;
+  user: User | null;
+  signin: (data: SigninPayload) => Promise<void>;
+  signup: (data: SignupPayload) => Promise<void>;
   signout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("access_token");
     if (token) {
       navigate("/user");
+      console.log(user);
+    } else {
+      navigate("/");
     }
-  }, []);
+  }, [user, navigate]);
 
-  const signin = async (email: string, password: string) => {
+  const signin = async (data: SigninPayload) => {
     console.log("[Auth] User signing in...");
 
-    const response = await signinUser({ email, password });
+    const response = await signinUser(data);
 
     if (response) {
       localStorage.setItem("access_token", response.access_token);
-      setUser(response.name);
+      localStorage.setItem("user", response.user);
+      setUser(response.user);
       console.log("[Auth] Signin success");
     } else {
       console.log("[Auth] Signin failed");
     }
   };
 
+  const signup = async (data: SignupPayload) => {
+    console.log("[Auth] User signing up...");
+
+    const response = await signupUser(data);
+    if (response?.status) {
+      if (response.status === "ok") {
+        console.log("[Auth] Signup success");
+      } else {
+        console.log("[Auth] Signup failed");
+      }
+    }
+  };
+
   const signout = () => {
     console.log("[Auth] User signing out...");
     localStorage.setItem("access_token", "");
+    localStorage.setItem("user", "");
     setUser(null);
+    navigate("/");
   };
 
   return (
-    <AuthContext.Provider value={{ user, signin, signout }}>
+    <AuthContext.Provider value={{ user, signin, signup, signout }}>
       {" "}
       {children}{" "}
     </AuthContext.Provider>
