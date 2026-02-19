8dbe80316515b923fac7a370bc464614b2b87894
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-19 20:35:54 +0800

    added a fallback UI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
-import AppRouter from "./router";
 import { ThemeProvider } from "../context/ThemeContext";
 import { AuthPageProvider } from "../context/AuthPageContext";
 import { AuthProvider } from "../context/AuthContext";
+
+import { Suspense, lazy } from "react";
+
+const AppRouter = lazy(() => import("./router"));
 
 function App() {
   console.log("Rendering root app");
@@ -10,7 +13,16 @@ function App() {
       <AuthProvider>
         <AuthPageProvider>
           <ThemeProvider>
-            <AppRouter />
+            <Suspense
+              fallback={
+                <div className="text-4xl font-bold dark:bg-white dark:text-black">
+                  {" "}
+                  Page Loading...
+                </div>
+              }
+            >
+              <AppRouter />
+            </Suspense>
           </ThemeProvider>
         </AuthPageProvider>
       </AuthProvider>
