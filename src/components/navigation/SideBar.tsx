8122ba173ebe0d290e26f0a6b22914a73ae656e1
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-19 20:39:54 +0800

    use .map() in repeatable codes

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,15 +1,17 @@
 import { IoSettings } from "react-icons/io5";
 import { MdDashboardCustomize } from "react-icons/md";
 import { BiSupport } from "react-icons/bi";
-import { NavLink, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import { NavLink } from "react-router-dom";
 
 const SideBar = () => {
-  const { signout } = useAuth();
-  const navigate = useNavigate();
+  const navLinks = [
+    { link: "/user", icon: <MdDashboardCustomize />, name: "Dashboard" },
+    { link: "/settings", icon: <IoSettings />, name: "Settings" },
+    { link: "/support", icon: <BiSupport />, name: "Support" },
+  ];
   return (
     <>
-      <div className="flex flex-col gap-2 p-4 bg-neutral-500 dark:bg-neutral-800 lg:w-[15vw] lg:h-full lg:px-3 lg:py-10 lg:gap-5 lg:text-md">
+      <aside className="flex flex-col gap-2 p-4 bg-neutral-400 dark:bg-neutral-800 lg:w-[15vw] lg:h-full lg:px-3 lg:py-10 lg:gap-5 lg:text-md">
         {/* LOGO */}
         <div className="flex gap-2 p-2 border-b-2 mb-2 border-neutral-700 lg:pb-5">
           <img src="src/assets/react.svg" alt="logo" />
@@ -18,40 +20,16 @@ const SideBar = () => {
           </h1>
         </div>
 
-        {/* DASHBOARD */}
-        <div className="flex gap-1 items-center">
-          <NavLink to="/user" className=" ">
-            <MdDashboardCustomize />
-          </NavLink>
-          <h3>Dashboard</h3>
-        </div>
-
-        {/* SETTINGS */}
-        <div className="flex gap-1 items-center">
-          <NavLink to="/settings" className=" ">
-            <IoSettings className=" " />
-          </NavLink>
-          <h3>Settings</h3>
-        </div>
-
-        {/* SUPPORT*/}
-        <div className="flex gap-1 items-center">
-          <NavLink to="/support" className=" ">
-            <BiSupport />
-          </NavLink>
-          <h3>Support</h3>
-        </div>
-
-        <div>
-          <button
-            onClick={() => {
-              signout();
-            }}
+        {navLinks.map((navLink, index) => (
+          <NavLink
+            key={index}
+            to={navLink.link}
+            className="flex gap-2 items-center "
           >
-            Logout
-          </button>
-        </div>
-      </div>
+            {navLink.icon} {navLink.name}
+          </NavLink>
+        ))}
+      </aside>
     </>
   );
 };
--- src/components/navigation/TopBar.tsx
+++ src/components/navigation/TopBar.tsx
@@ -1,6 +1,67 @@
 import { FaUserCircle, FaRegMoon, FaRegSun } from "react-icons/fa";
 import { MdMessage, MdCircleNotifications } from "react-icons/md";
+import { ImProfile } from "react-icons/im";
+import { IoLogOut } from "react-icons/io5";
+
 import { useTheme } from "../../context/ThemeContext";
+import { useAuth } from "../../context/AuthContext";
+import { useState, useEffect, useRef } from "react";
+
+interface ModalProp {
+  heading: string;
+  isOpen: boolean;
+  contents: string[];
+}
+
+const Modal = ({ isOpen, heading, contents }: ModalProp) => {
+  if (!isOpen) return null;
+  return (
+    <aside className="absolute  rounded p-4 bg-neutral-400 dark:bg-neutral-700">
+      <h1 className="border-b-1 pb-2 border-neutral-500 lg:text-xl ">
+        {heading}
+      </h1>
+      <div className="flex flex-col gap-2 items-start pt-2 lg:text-sm">
+        {contents.map((content, index) => (
+          <p key={index}>{content}</p>
+        ))}
+      </div>
+    </aside>
+  );
+};
+
+const UserModal = ({ isOpen }: ModalProp) => {
+  const { user, signout } = useAuth();
+
+  if (!isOpen) return null;
+
+  return (
+    <aside className="rounded-2xl p-4 bg-neutral-400 absolute dark:bg-neutral-700 ">
+      <h1 className="border-b-1 pb-2 border-neutral-800 dark:border-neutral-500 lg:text-xl ">
+        {user ? user.username : "User"}
+      </h1>
+      <div className="flex flex-col gap-2 items-start pt-2 lg:text-sm">
+        <button
+          className="flex gap-2 items-center"
+          onClick={() => {
+            signout();
+          }}
+        >
+          <ImProfile />
+          My Profile
+        </button>
+        <button
+          className="flex gap-2 items-center"
+          onClick={() => {
+            signout();
+          }}
+        >
+          <IoLogOut />
+          Logout
+        </button>
+      </div>
+    </aside>
+  );
+};
 
 const TopBar = () => {
   const { theme, setTheme } = useTheme();
@@ -8,24 +69,66 @@ const TopBar = () => {
     const nextTheme = theme === "light" ? "dark" : "light";
     setTheme(nextTheme);
   };
+  const [userModalOpen, setUserModalOpen] = useState(false);
+  const [messageModalOpen, setMessageModalOpen] = useState(false);
+  const [notifModalOpen, setNotifModalOpen] = useState(false);
+
+  const initialMessages = [
+    "Message01",
+    "Message02",
+    "Message03",
+    "Message04",
+    "Message05",
+  ];
+  const initialNotifs = ["Notif01", "Notif02", "Notif03"];
+
   return (
     <>
-      <header className="w-full h-[10vh] bg-white/80 dark:bg-black/80 lg:text-2xl lg:px-20 ">
+      <header className="w-full h-[10vh] lg:text-2xl lg:py-2 lg:px-10 lg:my-2">
         {/* */}
-        <div className="flex h-full justify-end items-center lg:gap-5">
-          <button>
-            <MdMessage />
+        <div className="flex h-full border border-neutral-400 dark:border-neutral-700 rounded-2xl justify-end items-center lg:gap-5 lg:px-10">
+          <button className="">
+            <MdMessage
+              onClick={() => {
+                setMessageModalOpen(!messageModalOpen);
+                setUserModalOpen(false);
+                setNotifModalOpen(false);
+              }}
+            />
+            <Modal
+              isOpen={messageModalOpen}
+              heading="Messages"
+              contents={initialMessages}
+            />
           </button>
           <button>
-            <MdCircleNotifications />
+            <MdCircleNotifications
+              onClick={() => {
+                setNotifModalOpen(!notifModalOpen);
+                setMessageModalOpen(false);
+                setUserModalOpen(false);
+              }}
+            />
+            <Modal
+              isOpen={notifModalOpen}
+              heading="Notifications"
+              contents={initialNotifs}
+            />
           </button>
           <button onClick={toggleTheme} className="lg:mx-5">
             {theme === "light" ? <FaRegMoon /> : <FaRegSun />}
           </button>
 
-          <div className="flex gap-1 items-center">
-            <FaUserCircle className="" />
-            <h1 className="lg:text-xl ">User</h1>
+          <div className="">
+            <FaUserCircle
+              className="lg:size-10"
+              onClick={() => {
+                setUserModalOpen(!userModalOpen);
+                setMessageModalOpen(false);
+                setNotifModalOpen(false);
+              }}
+            />
+            <UserModal isOpen={userModalOpen} />
           </div>
         </div>
       </header>
