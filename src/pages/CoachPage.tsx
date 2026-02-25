39c152898abc76af49b6cf63f97d7c75d07bbc46
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-25 20:51:16 +0800

    deleted

--- src/pages/CoachPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CoachPage = () => {
-  return <div>CoachPage</div>;
-};
-
-export default CoachPage;
--- src/pages/OwnerPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const OwnerPage = () => {
-  return <div>OwnerPage</div>;
-};
-
-export default OwnerPage;
