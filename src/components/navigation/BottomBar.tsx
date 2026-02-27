e02c53c90d20c6614c79d217328da583367c80fd
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-27 21:27:01 +0800

    will change to bottom navigations

--- src/components/navigation/BottomBar.tsx
+++ src/components/navigation/BottomBar.tsx
@@ -1,16 +1,22 @@
+import { MdDashboard } from "react-icons/md";
+
 import { NavLink } from "react-router-dom";
 
-const BottomBar = () => {
+const BottomBar = ({ navLinks }) => {
   return (
-    <footer className="h-[10vh] text-xs  px-3 text-neutral-800 dark:text-neutral-500 lg:px-5 lg:mt-0 lg:pt-7">
+    <footer className="h-[10vh] text-xs px-3 text-neutral-800 dark:text-neutral-500 lg:px-5 lg:mt-0 lg:pt-7 lg:hidden">
       <div className="border-t pt-2 content-center border-neutral-800 md:flex md:justify-between ">
-        <div>
-          <p>© {new Date().getFullYear()} All rights reserved.</p>
-        </div>
-        <div className="mt-2 flex justify-between md:mt-0 md:gap-4">
-          <NavLink to="/policy">Privacy Policy</NavLink>
-          <NavLink to="/terms">Terms of Service</NavLink>
-        </div>
+        <main className="flex gap-5 ">
+          {navLinks?.map((navLink, index) => (
+            <NavLink
+              key={index}
+              to={navLink.link}
+              className="flex gap-2 items-center justify-center border-b-1 border-neutral-700 lg:justify-start lg:pl-5 pb-2"
+            >
+              {navLink.icon} {navLink.name}
+            </NavLink>
+          ))}
+        </main>
       </div>
     </footer>
   );
