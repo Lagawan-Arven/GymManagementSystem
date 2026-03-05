a89715380852c795dfe6ff6f677f9b40651cc07b
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-03-05 20:41:06 +0800

    added more charts

---
+++ src/components/charts/StackAreaChart.tsx
@@ -0,0 +1,54 @@
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+import CustomTooltip from "./CustomTooltip";
+
+const StackAreaChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width={"100%"} height={"100%"} className={""}>
+      <AreaChart
+        data={data}
+        reverseStackOrder={true}
+        className="aspect-[1.618]"
+      >
+        <Area
+          dataKey={"session"}
+          stackId={1}
+          type={"monotone"}
+          fill="#22cbc9"
+        />
+        <Area dataKey={"member"} stackId={1} type={"monotone"} fill="#8522cb" />
+        <Area
+          dataKey={"walk_in"}
+          stackId={1}
+          type={"monotone"}
+          fill="#b29514"
+        />
+
+        <Area
+          dataKey={"new_member"}
+          stackId={1}
+          type={"monotone"}
+          fill="#17b214"
+        />
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey={"day"}
+          tick={{ className: "text-[0.7rem] lg:text-sm" }}
+        />
+        <YAxis tick={{ className: "text-[0.7rem] lg:text-sm" }} />
+
+        <Tooltip content={CustomTooltip} />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default StackAreaChart;
