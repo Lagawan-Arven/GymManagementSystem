189c6cc7998e9eeef439ec7b756e335765e8086d
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-03-05 20:40:43 +0800

    configure to be more reusable and accept stacking

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -10,19 +10,40 @@ import {
 } from "recharts";
 
 import CustomTooltip from "./CustomTooltip";
+import CustomLegend from "./util/CustomLegend";
 
-const MyBarChart = ({ data, barDataKey = "value", xAxisDataKey = "name" }) => {
+const COLORS = ["#d5fb2b", "#2ffb2b", "#2b77fb", "#8f2bfb", "#fb542b"];
+
+const MyBarChart = ({
+  data,
+  barDataKeys,
+  xAxisNameKey,
+}: {
+  data: any;
+  barDataKeys: string[];
+  xAxisNameKey: string;
+}) => {
   return (
-    <ResponsiveContainer height={"100%"} width={"100%"} style={{}}>
-      <BarChart
-        data={data}
-        barSize={15}
-        style={{ position: "relative" }}
-        responsive
-      >
-        <Bar dataKey={barDataKey} fill="#0088FE" activeBar={false} />
-        <XAxis dataKey={xAxisDataKey} fontSize={12} />
+    <ResponsiveContainer height={"100%"} width={"100%"}>
+      <BarChart data={data} barSize={15} reverseStackOrder={true} responsive>
+        {barDataKeys.map((barDataKey, index) => (
+          <Bar
+            key={index}
+            dataKey={barDataKey}
+            stackId={1}
+            fill={COLORS[index]}
+            activeBar={false}
+          />
+        ))}
+
+        <XAxis
+          dataKey={xAxisNameKey}
+          tick={{ className: "text-[0.7rem] lg:text-sm" }}
+        />
+        <YAxis tick={{ className: "text-[0.7rem] lg:text-sm" }} />
+        <CartesianGrid strokeDasharray={"3 3"} />
         <Tooltip content={CustomTooltip} cursor={false} />
+        <Legend formatter={CustomLegend} />
       </BarChart>
     </ResponsiveContainer>
   );
