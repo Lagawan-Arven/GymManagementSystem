16342301f508a315245aab05bab100f237dcacf7
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-03-05 20:41:27 +0800

    added custom legend for charts

---
+++ src/components/charts/util/CustomLegend.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomLegend = (value) => {
+  return (
+    <span className="text-xs md:text-sm text-black dark:text-white">
+      {value}
+    </span>
+  );
+};
+
+export default CustomLegend;
