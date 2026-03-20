6ef8a82ede004186c86e64f8a1df74866385835a
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-03-20 11:07:21 +0800

    deleted

--- src/pages/owner/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as OwnerHomePage } from "./OwnerHomePage";
-export { default as OwnerHomeDashboard } from "./OwnerHomeDashboard";
-export { default as OwnerHomeReports } from "./OwnerHomeReports";
-export { default as OwnerAdminsPage } from "./OwnerAdminsPage";
-export { default as OwnerMembersPage } from "./OwnerMembersPage";
-export { default as OwnerLogsPage } from "./OwnerLogsPage";
