1e789536362088e510a9143c2fc617e10b77f1c4
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-04-14 15:52:47 +0800

    feat: added theme toggle

--- src/components/layout/PublicNavbar.tsx
+++ src/components/layout/PublicNavbar.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-import { Dumbbell } from "lucide-react";
+import { Dumbbell, Sun, Moon } from "lucide-react";
 import { Button } from "../ui/button";
 
+import { useTheme } from "../../context/ThemeContext";
+
 export const PublicNavbar = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <header className="border-border/40 bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
@@ -35,6 +38,15 @@ export const PublicNavbar = () => {
 
         {/* Auth Actions */}
         <div className="flex items-center gap-4">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            <Sun className="h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Moon className="absolute h-5 w-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
           <Link
             to="/login"
             className="text-muted-foreground hover:text-foreground hidden text-sm font-medium transition-colors sm:block"
